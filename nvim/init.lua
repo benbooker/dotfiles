c2548ece33f43f8f96f6953d74ec4a33bf8448d6
Ben Booker <38851276+benbooker@users.noreply.github.com>
2024-12-14 19:34:02 +1100

    setup vim-tmux-navigator

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = ' '
 
+-- Lazy.nvim setup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -15,29 +16,41 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 
+-- Set theme
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
 
+-- General settings
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 3
-vim.opt.tabstop = 2
 
--- nvim-tree binds 
-vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+-- nvim-tree bindings
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
--- telescope binds
+-- Dynamic navigation for Neovim splits and Tmux panes
+local in_tmux = os.getenv("TMUX") ~= nil
+
+if in_tmux then
+  -- Use Tmux navigator if in Tmux
+  vim.keymap.set('n', '<C-h>', '<Cmd>TmuxNavigateLeft<CR>', { noremap = true, silent = true })
+  vim.keymap.set('n', '<C-j>', '<Cmd>TmuxNavigateDown<CR>', { noremap = true, silent = true })
+  vim.keymap.set('n', '<C-k>', '<Cmd>TmuxNavigateUp<CR>', { noremap = true, silent = true })
+  vim.keymap.set('n', '<C-l>', '<Cmd>TmuxNavigateRight<CR>', { noremap = true, silent = true })
+else
+  -- Default split navigation when not in Tmux
+  vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+  vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+  vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+  vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+end
+
+-- Telescope bindings
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { noremap = true, silent = true })
